02b37afb6c2d3ceececccff79f56b3849196596d
Deepanshu shekhar <dshekhar950@gmail.com>
2024-07-26 19:50:04 +0530

    Initial Upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // Corrected import
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/WeatherApp/"> {/* Corrected capitalization */}
+      <Routes>
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
+
